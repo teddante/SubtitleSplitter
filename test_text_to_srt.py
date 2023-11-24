cb2964d5280a06d430b4babe13efa4c8c316b919
Ted Enston <37670093+teddante@users.noreply.github.com>
2023-11-24 22:52:54 +0000

    Update test_text_to_srt.py

--- test_text_to_srt.py
+++ test_text_to_srt.py
@@ -2,7 +2,7 @@ import unittest
 from text_to_srt import (read_text_file, split_into_sentences, pair_sentences,
                                 calculate_display_times, format_timecode, format_subtitles)
 
-class TestSubtitleConverter(unittest.TestCase):
+class TestTextToSRT(unittest.TestCase):
 
     def test_pair_sentences(self):
         sentences = ['Sentence one.', 'Sentence two.', 'Sentence three.', 'Sentence four.']
@@ -21,7 +21,32 @@ class TestSubtitleConverter(unittest.TestCase):
         with self.assertRaises(ValueError):
             calculate_display_times(sentence_pairs, 0)
 
-    # Add more tests for other functions like format_timecode, format_subtitles, etc.
+    def test_read_text_file(self):
+        with open('test_file.txt', 'w') as f:
+            f.write('This is a test file.')
+        self.assertEqual(read_text_file('test_file.txt'), 'This is a test file.')
+        with self.assertRaises(IOError):
+            read_text_file('non_existent_file.txt')
 
+    def test_split_into_sentences(self):
+        text = 'This is sentence one. This is sentence two! Is this sentence three?'
+        self.assertEqual(split_into_sentences(text), ['This is sentence one.', 'This is sentence two!', 'Is this sentence three?'])
+        with self.assertRaises(ValueError):
+            split_into_sentences(123)
+
+    def test_format_timecode(self):
+        self.assertEqual(format_timecode(3661.123), '01:01:01,123')
+        with self.assertRaises(RuntimeError):
+            format_timecode('invalid input')
+
+    def test_format_subtitles(self):
+        sentence_pairs = [['Sentence one.', 'Sentence two.'], ['Sentence three.', 'Sentence four.']]
+        display_times = [1, 2]
+        expected_output = ['1\n00:00:00,000 --> 00:00:01,000\nSentence one. Sentence two.\n', 
+                           '2\n00:00:01,000 --> 00:00:03,000\nSentence three. Sentence four.\n']
+        self.assertEqual(format_subtitles(sentence_pairs, display_times), expected_output)
+        with self.assertRaises(RuntimeError):
+            format_subtitles('invalid input', display_times)
+            
 if __name__ == '__main__':
     unittest.main()
