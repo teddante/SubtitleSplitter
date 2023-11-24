2eb9c28ce50e72cd6e14bc680d96811f0c0ab663
Ted Enston <37670093+teddante@users.noreply.github.com>
2023-11-24 21:23:30 +0000

    Update text-to-srt.py

--- text-to-srt.py
+++ text-to-srt.py
@@ -1,21 +1,18 @@
 import re
+import argparse
 
 def read_text_file(file_path):
-    # Read the text file and return the content.
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
 def split_into_sentences(text):
-    # Use regular expression to split text into sentences.
     sentences = re.split(r'(?<=[.!?]) +', text)
     return sentences
 
-def pair_sentences(sentences):
-    # Pair the sentences into groups of two.
-    return [sentences[i:i + 2] for i in range(0, len(sentences), 2)]
+def pair_sentences(sentences, num_sentences_per_group):
+    return [sentences[i:i + num_sentences_per_group] for i in range(0, len(sentences), num_sentences_per_group)]
 
 def calculate_display_times(sentence_pairs, words_per_minute):
-    # Calculate display times for each pair of sentences.
     display_times = []
     for pair in sentence_pairs:
         word_count = len(' '.join(pair).split())
@@ -23,38 +20,40 @@ def calculate_display_times(sentence_pairs, words_per_minute):
         display_times.append(display_time_seconds)
     return display_times
 
+def format_timecode(seconds):
+    hours, seconds = divmod(seconds, 3600)
+    minutes, seconds = divmod(seconds, 60)
+    milliseconds = int(seconds * 1000 % 1000)
+    return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02},{milliseconds:03}"
+
 def format_subtitles(sentence_pairs, display_times):
-    # Format the pairs into SRT format with display times.
     subtitles = []
     start_time = 0
     for index, (pair, duration) in enumerate(zip(sentence_pairs, display_times)):
         end_time = start_time + duration
         start_timecode = format_timecode(start_time)
         end_timecode = format_timecode(end_time)
-        subtitle_text = ' '.join(pair)
+        subtitle_text = ' '.join(pair).replace('\n', ' ')  # Replace newlines with spaces
         subtitles.append(f"{index+1}\n{start_timecode} --> {end_timecode}\n{subtitle_text}\n")
-        start_time = end_time
+        start_time += duration  # Increment start_time for the next pair
     return subtitles
 
-def format_timecode(seconds):
-    # Convert seconds to SRT timecode format.
-    hours, seconds = divmod(seconds, 3600)
-    minutes, seconds = divmod(seconds, 60)
-    return f"{int(hours):02}:{int(minutes):02}:{int(seconds):02},000"
-
 def save_subtitle_file(subtitles, output_file_path):
-    # Save the subtitles to a file.
     with open(output_file_path, 'w', encoding='utf-8') as file:
         file.write('\n'.join(subtitles))
 
-# Usage example:
-file_path = 'path/to/text/file.txt'
-output_file_path = 'path/to/output/file.srt'
-words_per_minute = 150  # The average reading speed in words per minute.
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert text to subtitle file with customizable sentence grouping.")
+    parser.add_argument('file_path', help='Path to the input text file')
+    parser.add_argument('output_file_path', help='Path to the output subtitle file')
+    parser.add_argument('words_per_minute', type=int, help='Average reading speed in words per minute')
+    parser.add_argument('--sentences_per_caption', type=int, default=2, help='Number of sentences per subtitle caption')
 
-text_content = read_text_file(file_path)
-sentences = split_into_sentences(text_content)
-sentence_pairs = pair_sentences(sentences)
-display_times = calculate_display_times(sentence_pairs, words_per_minute)
-subtitles = format_subtitles(sentence_pairs, display_times)
-save_subtitle_file(subtitles, output_file_path)
+    args = parser.parse_args()
+
+    text_content = read_text_file(args.file_path)
+    sentences = split_into_sentences(text_content)
+    sentence_pairs = pair_sentences(sentences, args.sentences_per_caption)
+    display_times = calculate_display_times(sentence_pairs, args.words_per_minute)
+    subtitles = format_subtitles(sentence_pairs, display_times)
+    save_subtitle_file(subtitles, args.output_file_path)
