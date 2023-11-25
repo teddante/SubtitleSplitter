c1bcdb28afa634f3b32411ccb1c9d6fff1a007be
Ted Enston <37670093+teddante@users.noreply.github.com>
2023-11-25 20:16:28 +0000

    Bring in C# project and new ignore

---
+++ SubtitleSplitter/SubtitleSplitter/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SubtitleSplitter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
